058fe1fa8fc0dfbea8e635e2119b83c253f1810e
Dhiman-neh <nehadhiman.softprodigy@gmail.com>
2022-05-04 18:38:17 +0530

    my-code

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+// const uri = 'https://80de-2401-4900-1c1b-1102-159c-e952-c805-2da7.in.ngrok.io/graphql'; // <-- add the URL of the GraphQL server here
 
-const uri = 'https://ca55-182-75-107-70.in.ngrok.io/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'https://4fb2-182-75-107-70.in.ngrok.io/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
